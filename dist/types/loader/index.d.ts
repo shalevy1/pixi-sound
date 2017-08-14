fce4b2d6d2b76c013b2f113e42fa9147d081465b
Matt Karl <matt.karl@jibo.com>
2017-08-14 17:17:28 -0400

    Updates

--- dist/types/loader/index.d.ts
+++
@@ -1,8 +0,0 @@
-import SoundLibrary from "../SoundLibrary";
-export default class LoaderMiddleware {
-    static _sound: SoundLibrary;
-    static install(sound: SoundLibrary): void;
-    static legacy: boolean;
-    private static resolve(resource, next);
-    private static plugin(resource, next);
-}
